0d603924516db29bcf2511206bf01204d4634521
Mahmoodreza Aarabi <madoodia@gmail.com>
2015-03-04 10:47:58 +0330

    another challenge from codeeval

---
+++ python/codeeavl.com/Easy/Multiplication Tables/README.md
@@ -0,0 +1,3 @@
+# Codeeval.com challengs [Easy Level]
+# Multiplication Tables
+[**Link**](https://www.codeeval.com/open_challenges/23)
---
+++ python/codeeavl.com/Easy/Multiplication Tables/solution_01.py
@@ -0,0 +1,8 @@
+# Multiplication Tables Challenge
+
+
+if __name__ == "__main__":
+    for i in range(1, 13):
+        for j in range(1, 13):
+            print "%3d" % (j * i),
+        print ""
