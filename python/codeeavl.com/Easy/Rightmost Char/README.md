311536b81440d9fd485b09bb69121abc6906f447
Mahmoodreza Aarabi <madoodia@gmail.com>
2015-03-05 11:54:18 +0330

    another challenge from codeeval

---
+++ python/codeeavl.com/Easy/Rightmost Char/README.md
@@ -0,0 +1,3 @@
+# Codeeval.com challengs [Easy Level]
+# Rightmost Char
+[**Link**](https://www.codeeval.com/open_challenges/31)
---
+++ python/codeeavl.com/Easy/Rightmost Char/solution.py
@@ -0,0 +1,10 @@
+# Rightmost Char Challenge
+
+
+if __name__ == "__main__":
+    import sys
+    with open(sys.argv[1]) as f:
+        for line in f.readlines():
+            str1 = line.strip().split(',')[0]
+            t = line.strip().split(',')[1]
+            print str1.rfind(t)
