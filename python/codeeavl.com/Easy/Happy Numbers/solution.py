344add784aa4576a561b9f2e54c4f2b273d8d0ed
Mahmoodreza Aarabi <madoodia@gmail.com>
2015-03-06 18:46:22 +0330

    Another challenge from codeeval

--- python/codeeavl.com/Easy/Happy Numbers/solution.py
+++ python/codeeavl.com/Easy/Happy Numbers/solution.py
@@ -3,8 +3,32 @@
 
 if __name__ == "__main__":
     import sys
+    import math
+
+    def happy(sep):
+        res = 0
+        for i in sep:
+            res += math.pow(i, 2)
+        return int(res)
+
+    def count(n):
+        sep = []
+        while n / 10 != 0:
+            sep.append(n % 10)
+            n = n / 10
+        sep.append(n)
+        return sep
+
     with open(sys.argv[1]) as f:
         for line in f.readlines():
-            str1 = line.strip().split(',')[0]
-            t = line.strip().split(',')[1]
-            print str1.rfind(t)
+            c = 0
+            base = int(line.strip())
+            num = int(line.strip())
+            while num != 1 and c < 10:
+                sep = count(num)
+                num = happy(sep)
+                c += 1
+            if num == 1:
+                print 1
+            else:
+                print 0
