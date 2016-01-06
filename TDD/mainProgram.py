3b34c1b44a9583ce638fe36ba71399eac5b0e837
madoodia <madoodia@gmail.com>
2016-01-06 11:54:38 +0330

    Starting Learning TDD

---
+++ TDD/mainProgram.py
@@ -0,0 +1,8 @@
+# this is our main program that should be tested
+# ----------------------------------------------- #
+
+
+def some_method(x):
+    if x > 5:
+        return True
+    return False
---
+++ TDD/unitTest_bankAccountProgram.py
@@ -0,0 +1,12 @@
+
+# create a new BankAccount object
+# deposit 50
+# check balance is 50
+
+# withdraw 30
+# check balance is 20
+
+# what about negative amounts?
+# any limits on max deposit?
+
+# ------------------------------------------ #
